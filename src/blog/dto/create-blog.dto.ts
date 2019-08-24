823c6735b6aee5a6b9b833c6403396302b1eeed5
Vefa Çağlar Özcan <vefacaglarozcan@gmail.com>
2019-08-24 17:15:34 +0300

    forgot swagger model

--- src/blog/dto/create-blog.dto.ts
+++ src/blog/dto/create-blog.dto.ts
@@ -1,7 +1,25 @@
+import { ApiModelProperty } from '@nestjs/swagger'
+import { IsString, IsDate, IsBoolean } from 'class-validator'
+
 export class CreateBlogDto {
+
+    @ApiModelProperty()
+    @IsString()
     readonly title: string;
+
+    @ApiModelProperty()
+    @IsString()
     readonly author: string;
+
+    @ApiModelProperty()
+    @IsString()
     readonly body: string;
+
+    @ApiModelProperty()
+    @IsDate()
     readonly date: Date;
+
+    @ApiModelProperty()
+    @IsBoolean()
     readonly hidden: boolean;
 }
